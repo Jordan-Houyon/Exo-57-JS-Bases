7137a6be365cbf3b1b3c713a1160e845bc8012c7
jordan houyon <116800565+Jordan-Houyon@users.noreply.github.com>
2022-11-20 20:15:10 +0100

    exo 57 ok

---
+++ exo 5/main.js
@@ -0,0 +1,8 @@
+let numnberOne= prompt("Saisir un nombre à virgule");
+let numberTwo= prompt("Saisir un second nombre à virgule");
+
+parseInt(numnberOne);
+
+let resultat= numnberOne * numberTwo;
+
+alert(resultat);
